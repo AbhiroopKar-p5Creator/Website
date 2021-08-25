324615be07b55c15b7e62bf5e701f2ae710cb5ae
AbhiroopKar-p5Creator <71604858+AbhiroopKar-p5Creator@users.noreply.github.com>
2021-08-25 12:35:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,5 +13,5 @@ function draw() {
   background(1,200,150);
   color(0,0,0)
   text("Kaka Website",Width,100)
-  let a = createA('https://drive.google.com/uc?export=download&id=16krCF89JfBw5MH9mU4wqClQn5whn5xDq', 'DownloadSample');
-  a.position(Width, 200);}
+  let a = createA('https://drive.google.com/uc?export=download&id=16krCF89JfBw5MH9mU4wqClQn5whn5xDq', 'DownloadSample')
+  a.position(Width, 200)
