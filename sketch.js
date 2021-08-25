210fdae69919374495437126e3e1b5734d1248d5
AbhiroopKar-p5Creator <clashandbrawlgaming2020@gmail.com>
2021-08-25 13:37:50 +0530

    One Commit

--- sketch.js
+++ sketch.js
@@ -1,16 +1,25 @@
 var Width 
 var Height
+var comicFont
 
-function setup() {
-  createCanvas(1366 ,677);
+function setup()
+{
+ createCanvas(1366 ,677);
 }
 
-function draw() {
+function preload()
+{
+  comicFont = loadFont('Assets/ComicSans.ttf');
+}
+
+function draw() 
+{
   Height = window.height/ 2
-  Width = window.width / 2
-  
+  Width = window.width / 2  
   
   background(1,200,150);
   color(0,0,0)
-  text("Kaka Website",Width,100)
+  textSize(30)
+  textFont(comicFont);
+  text("Sudipta Duttta Roy",Width,100)
 }
