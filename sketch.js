8103f7a4a11a66ea4531cf9ea265f5cdcfc3a678
AbhiroopKar-p5Creator <clashandbrawlgaming2020@gmail.com>
2021-08-25 12:23:56 +0530

    Upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var Width 
+var Height
+
+function setup() {
+  createCanvas(1366 ,677);
+}
+
+function draw() {
+  Height = window.height/ 2
+  Width = window.width / 2
+  
+  
+  background(1,200,150);
+  color(0,0,0)
+  text("Kaka Website",Width,100)
+}
